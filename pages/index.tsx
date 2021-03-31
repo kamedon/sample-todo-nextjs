41a6659d1df3392c075c43b059aa5eee73d370e6
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2021-03-31 18:59:56 +0900

    リファクタリング

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Layout from '../components/Layout'
+import Layout from '../src/components/template/Layout'
 import TodoPage from "../src/components/page/TodoPage";
 import {NextPage} from "next";
 
